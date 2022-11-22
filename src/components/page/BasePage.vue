0dcd7f41d9fe2a3512baa29d89dd9af62c31f3d9
Lucas Vinotti <luckmfv@gmail.com>
2022-11-21 23:23:23 -0300

    feat: create base page component

---
+++ src/components/page/BasePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="page" class="page">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, watch } from 'vue'
+
+export default defineComponent({
+  name: 'BasePage',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    watch(
+      () => props.title,
+      (newTitle) => {
+        document.title = newTitle
+      }
+    )
+  }
+})
+</script>
