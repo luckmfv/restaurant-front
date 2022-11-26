16712f63d32ae27bb6d691f312a94d35f6e691ba
Lucas Vinotti <luckmfv@gmail.com>
2022-11-26 17:51:47 -0300

    feat: add function to create account

--- src/modules/onboarding/services/auth.service.ts
+++ src/modules/onboarding/services/auth.service.ts
@@ -1,9 +1,11 @@
-import type { LoginFormDto } from '../dtos'
+import type { CreateAccountFormDto, LoginFormDto } from '../dtos'
 
 class AuthService {
   public async signIn(_loginFormDto: LoginFormDto): Promise<string> {
     return 'token_'
   }
+
+  public async createAccount(_createAccountFormDto: CreateAccountFormDto): Promise<void> {}
 }
 
 export const authService = new AuthService()
