44c722e6af609caa55b44b99b8a3dfadc8b158fb
Lucas Vinotti <luckmfv@gmail.com>
2022-11-26 17:30:02 -0300

    feat: add class into tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        base: ['Ubuntu', 'Arial', 'sans-serif'],
+        title: ['Comfortaa', 'Arial', 'sans-serif']
+      },
+      fontSize: {
+        title: {
+          fontSize: '1.25rem',
+          lineHeight: '1.75rem'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
