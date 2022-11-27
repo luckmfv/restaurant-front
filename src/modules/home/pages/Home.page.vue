a8af71852afc9b3e0d859aa935f1fe58ad5033fa
Lucas Vinotti <luckmfv@gmail.com>
2022-11-27 00:27:47 -0300

    feat: add home page

---
+++ src/modules/home/pages/Home.page.vue
@@ -0,0 +1,17 @@
+<template>
+  <BasePage title="Home">
+    Home
+  </BasePage>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeHome.page',
+  setup() {
+
+    return {}
+  }
+})
+</script>
