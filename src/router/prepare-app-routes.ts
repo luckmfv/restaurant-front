0096eb302bbe71a4131e2455bab307ee9a13246a
Lucas Vinotti <luckmfv@gmail.com>
2022-11-27 00:27:33 -0300

    feat: verify authtoken before routes

--- src/router/prepare-app-routes.ts
+++ src/router/prepare-app-routes.ts
@@ -1,12 +1,14 @@
 import { useAuthStorage } from '@/modules/app/compositions'
 import type { Router } from 'vue-router'
 
-const { hasAuthToken } = useAuthStorage()
+const { verifyIfHasAuthToken } = useAuthStorage()
 
 export const prepareAppRoutes = (router: Router): void => {
   router.beforeEach((to, _from, next) => {
-    if (to.meta.requireAuth) {
-      hasAuthToken.value ? next() : next({ name: 'auth.login' })
+    if (to.meta.requireAuth && !verifyIfHasAuthToken()) {
+      next({ name: 'auth.login' })
     }
+
+    next()
   })
 }
