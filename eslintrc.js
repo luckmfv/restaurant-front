14eddcef375f2a09850753e4ca0da6141bf86608
Lucas Vinotti <luckmfv@gmail.com>
2022-12-18 16:31:14 -0300

    chore: edit eslint rc

---
+++ eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/vue3-essential', '@vue/typescript', '@vue/standard'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'no-shadow': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-fallthrough': 'off',
+    'space-before-function-paren': ['error', 'always'],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }
+    ],
+    'no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }
+    ],
+    '@typescript-eslint/no-shadow': ['error'],
+    'quotes': ['error', 'simple']
+  }
+}
