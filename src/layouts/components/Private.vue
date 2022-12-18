0c69f3f6f3374356ae473bed064333932e683f44
Lucas Vinotti <luckmfv@gmail.com>
2022-12-18 16:34:54 -0300

    feat: add sidebar to private layout

--- src/layouts/components/Private.vue
+++ src/layouts/components/Private.vue
@@ -1,11 +1,52 @@
 <template>
-  <RouterView />
+  <div id="private-layout">
+    <div v-if="!shouldRenderApplication">
+      loading
+    </div>
+    <div
+      v-else
+      class="flex"
+    >
+      <BaseSidebar />
+      <RouterView />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { applicationService } from '@/modules/app/services'
+import { useProfile, useLoading } from '@/modules/app/compositions'
+import { computed, defineComponent, onBeforeMount } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
-  name: 'layout-private'
+  name: 'layout-private',
+  setup() {
+    const router = useRouter()
+    const [applicationIsLoading, startApplicationLoading, stopApplicationLoading] = useLoading()
+    const { hasProfile, profile } = useProfile()
+
+    const shouldRenderApplication = computed(() => {
+      return !applicationIsLoading.value
+    })
+
+    onBeforeMount(async() => {
+      if (!hasProfile.value) {
+        startApplicationLoading()
+        const profileWasSet = await applicationService.setProfile()
+
+        if (!profileWasSet) {
+          router.push({ name: 'auth.login' })
+        }
+
+        stopApplicationLoading()
+      }
+    })
+
+    return {
+      shouldRenderApplication,
+      profile
+    }
+  }
 })
 </script>
--- src/plugins/font-awesome/font-awesome.install.ts
+++ src/plugins/font-awesome/font-awesome.install.ts
@@ -1,10 +1,10 @@
-import { faUser, faUserFriends, faFileInvoiceDollar, faStore } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faUserFriends, faFileInvoiceDollar, faStore, faChevronLeft, faChevronRight, faBowlFood, faUtensils, faBagShopping, faBox } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import type { App } from 'vue'
 
 
 export const fontAwesomeInstall = (app: App): void => {
-  library.add(faUser, faUserFriends, faFileInvoiceDollar, faStore)
+  library.add(faUser, faUserFriends, faFileInvoiceDollar, faStore, faChevronLeft, faChevronRight, faBowlFood, faUtensils, faBagShopping, faBox)
   app.component('FontAwesomeIcon', FontAwesomeIcon)
 }
