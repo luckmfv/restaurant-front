e8ea12cb5ad35253c6c3db66378078eb901a5def
Lucas Vinotti <luckmfv@gmail.com>
2022-12-20 21:24:41 -0300

    refactor: add props to dto

--- src/modules/auth/dtos/create-account-form.dto.ts
+++ src/modules/auth/dtos/create-account-form.dto.ts
@@ -1,11 +1,31 @@
+export class CreateAccountContactsFormDto {
+  phone: string
+  email: string
+
+  static blankForm(): CreateAccountContactsFormDto {
+    const createAccountContactsFormDto = new CreateAccountContactsFormDto()
+
+    createAccountContactsFormDto.phone = ''
+    createAccountContactsFormDto.email = ''
+
+    return createAccountContactsFormDto
+  }
+}
+
 export class CreateAccountFormDto {
-  username: string
+  documentNumber: string
+  legalName: string
+  socialName: string
+  contacts: CreateAccountContactsFormDto
   password: string
 
   static blankForm(): CreateAccountFormDto {
     const createAccountFormDto = new CreateAccountFormDto()
 
-    createAccountFormDto.username = ''
+    createAccountFormDto.legalName = ''
+    createAccountFormDto.socialName = ''
+    createAccountFormDto.documentNumber = ''
+    createAccountFormDto.contacts = CreateAccountContactsFormDto.blankForm()
     createAccountFormDto.password = ''
 
     return createAccountFormDto
